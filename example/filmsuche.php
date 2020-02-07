8b71e83f3837bf4a38f4ae9fbf6a3c461a50a9d1
Bluuax <Bluuax@users.noreply.github.com>
2020-02-07 19:53:35 +0100

    Update stuff

--- example/filmsuche.php
+++ example/filmsuche.php
@@ -14,81 +14,94 @@
     <title>Filmsuche</title>
   </head>
   <body>
-    <div class="custom-container">
-      <h3>Suchergebnis</h3>
-      <?php
-        if (!empty($_GET["input"])) {
-          $connection = new PDO("mysql:host=localhost;dbname=filmauswahl", "root", "");
-          $connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    <div  class="custom-container">
+      <nav>
+        <ul class="nav nav-tabs">
+          <li class="nav-item">
+            <a class="nav-link active" href="filmsuche.html">Filmsuche</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="schauspielersuche.html">Schauspielersuche</a>
+          </li>
+        </ul>
+      </nav>
 
-          $param = "%" . $_GET["input"] . "%";
+      <div class="content-container">
+        <h3>Suchergebnis</h3>
+        <?php
+          if (isset($_GET["input"]) && $_GET["input"] != "") {
+            $connection = new PDO("mysql:host=localhost;dbname=filmauswahl", "root", "");
+            $connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
-          $statement = $connection->prepare(
-            "SELECT Name FROM produktionsfirma
-            WHERE Name LIKE :input;"
-          );
-          $statement->bindParam(':input', $param);
-          $statement->execute();
-          $result = $statement->fetchAll();
-          // echo "<script>console.log('" . json_encode($result) . "');</script>";
-          ?>
-
-          <p>Gesuchte Produktionsfirma: <span><?php echo $_GET["input"] ?></span></p>
-        
-          <?php
-          if (sizeof($result) == 0) {
-            echo "<p>Produktionsfirma nicht gefunden</p>";
-          } else {
-            echo "<p>Gefundene Produktionsfirma/Produktionsfirmen: ";
-            $resultString = "";
-            foreach ($result as $row) {
-              $resultString = $resultString . $row["Name"] . ", ";
-            }
-            echo "<span>" . substr($resultString, 0, -2) . "</span></p>";
+            $param = "%" . $_GET["input"] . "%";
 
             $statement = $connection->prepare(
-              "SELECT film.Name AS Titel, film.Erscheinungsdatum AS Erscheinungsdatum, produktionsfirma.Name AS Produktionsfirma
-              FROM film
-              LEFT JOIN produktionsfirma
-              ON film.Produktionsfirma_Id = produktionsfirma.Id
-              WHERE produktionsfirma.Name LIKE :input
-              ORDER BY film.Erscheinungsdatum asc;"
+              "SELECT Name FROM produktionsfirma
+              WHERE Name LIKE :input;"
             );
             $statement->bindParam(':input', $param);
             $statement->execute();
             $result = $statement->fetchAll();
+            // echo "<script>console.log('" . json_encode($result) . "');</script>";
+            ?>
 
-            echo "<p>Gefundene Filmtitel: <span>" . sizeof($result) . "</span></p>";
-
-            if (sizeof($result) > 0) {
-              ?>
-              <table class="table">
-                <thead>
-                  <tr>
-                    <th scope="col">Titel</th>
-                    <th scope="col">Erscheinungsdatum</th>
-                    <th scope="col">Produktionsfirma</th>
-                  </tr>
-                </thead>
-                <tbody>
-                  <?php 
-                    foreach ($result as $row) {
-                      echo "<tr>";
-                        echo "<td>" . $row["Titel"] . "</td>";
-                        echo "<td>" . date("m.d.Y", strtotime($row["Erscheinungsdatum"])) . "</td>";
-                        echo "<td>" . $row["Produktionsfirma"] . "</td>";
-                      echo "</tr>";
-                    }
-                  ?>
-                </tbody>
-              </table>
-              <?php
+            <p>Gesuchte Produktionsfirma: <span><?php echo $_GET["input"] ?></span></p>
+          
+            <?php
+            if (sizeof($result) == 0) {
+              echo "<p>Produktionsfirma nicht gefunden</p>";
+            } else {
+              echo "<p>Gefundene Produktionsfirma/Produktionsfirmen: ";
+              $resultString = "";
+              foreach ($result as $row) {
+                $resultString = $resultString . $row["Name"] . ", ";
               }
-            }
-        } else {
-          echo "<p>Es wurde keine Produktionsfirma mitgegeben</p>";
-        }
-      ?>
+              echo "<span>" . substr($resultString, 0, -2) . "</span></p>";
+
+              $statement = $connection->prepare(
+                "SELECT film.Name AS Titel, film.Erscheinungsdatum AS Erscheinungsdatum, produktionsfirma.Name AS Produktionsfirma
+                FROM film
+                LEFT JOIN produktionsfirma
+                ON film.Produktionsfirma_Id = produktionsfirma.Id
+                WHERE produktionsfirma.Name LIKE :input
+                ORDER BY film.Erscheinungsdatum asc;"
+              );
+              $statement->bindParam(':input', $param);
+              $statement->execute();
+              $result = $statement->fetchAll();
+
+              echo "<p>Gefundene Filmtitel: <span>" . sizeof($result) . "</span></p>";
+
+              if (sizeof($result) > 0) {
+                ?>
+                <table class="table">
+                  <thead>
+                    <tr>
+                      <th scope="col">Titel</th>
+                      <th scope="col">Erscheinungsdatum</th>
+                      <th scope="col">Produktionsfirma</th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    <?php 
+                      foreach ($result as $row) {
+                        echo "<tr>";
+                          echo "<td>" . $row["Titel"] . "</td>";
+                          echo "<td>" . date("m.d.Y", strtotime($row["Erscheinungsdatum"])) . "</td>";
+                          echo "<td>" . $row["Produktionsfirma"] . "</td>";
+                        echo "</tr>";
+                      }
+                    ?>
+                  </tbody>
+                </table>
+                <?php
+                }
+              }
+          } else {
+            echo "<p>Es wurde keine Produktionsfirma mitgegeben</p>";
+          }
+        ?>
+      </div>
     </div>
   </body>
 </html>
